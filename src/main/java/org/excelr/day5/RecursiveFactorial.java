198507f25c13b400e21ce129273317380fba5706
LAKSHMIKANT DESHPANDE <fullstacktrainer4u@gmail.com>
2023-11-15 21:02:50 +0530

    changes

--- src/main/java/org/excelr/day5/RecursiveFactorial.java
+++ src/main/java/org/excelr/day5/RecursiveFactorial.java
@@ -4,7 +4,7 @@ public class RecursiveFactorial {
 
     public static int factorial(int n) {
         // Base case: factorial of 0 is 1
-        if (n == 0) {
+        if (n == 0 || n == 1) { // 0! = 1
             return 1;
         } else {
             // Recursive case: n! = n * (n-1)!
